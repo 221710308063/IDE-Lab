2cee3a666d0e33a9cbbced40f2b6bed3a6d00f5e
adithya134 <saiadu134@gmail.com>
2020-09-02 18:26:00 +0530

    commit

---
+++ Numbers/1.cs
@@ -0,0 +1,10 @@
+int a = 18;
+int b = 6;
+int c = a + b;
+int d = a - b;
+int e = a/b;
+int f = a*b;
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.WriteLine(f);
---
+++ Numbers/3.cs
@@ -0,0 +1,5 @@
+int a = 5;
+int b = 4;
+int c = 2;
+int d = (a + b) * c;
+Console.WriteLine(d);
